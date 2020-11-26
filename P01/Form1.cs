507d773feba9fa00d8af8c2f98db1ce2c831810c
Spajda65 <spacekt.03@spst.eu>
2020-11-26 19:23:51 +0100

    Přidání Příkladu 1

---
+++ P01/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string zprava = "";
+            foreach (char pis in textBox1.Text)
+            {
+                if (pis != ' ' && pis != '!' && pis != '.' && pis != '?' && pis != ',')
+                    zprava += (char)(pis + 3);
+                else zprava += pis;
+            }
+            MessageBox.Show(zprava);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string pismena = "QWERTZUIOPASDFGHJKLYXCVBNM . , ! ?";
+            int i = 0;
+            foreach (Control ctrl in panel1.Controls)
+            {
+                if (ctrl is Button) (ctrl as Button).Text = pismena[i].ToString();
+                i++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += (sender as Button).Text;
+        }
+    }
+}
